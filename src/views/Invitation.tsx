65980c4e246819c3757912f6e3bf5b8677abba4c
unclejames2610 <udealaebuka@gmail.com>
2025-08-11 16:49:42 +0100

    chore:invitation page start

---
+++ src/views/Invitation.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const Invitation = () => {
+  return <div className="flex flex-col w-full min-h-screen relative"></div>;
+};
+
+export default Invitation;
